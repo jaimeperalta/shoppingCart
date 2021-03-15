19db3743cbb3cdfda5c04eac2c788b100716145c
jaimeperalta <andrescharris4@gmail.com>
2021-03-15 00:51:52 -0500

    build publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCa_4xtuMLxdR6UCcz38PlFEvxHVMCene0",
+    authDomain: "shopping-cart-66c74.firebaseapp.com",
+    projectId: "shopping-cart-66c74",
+    storageBucket: "shopping-cart-66c74.appspot.com",
+    messagingSenderId: "374586192698",
+    appId: "1:374586192698:web:95b60df9abeecac87a1603"
+  }
 };
